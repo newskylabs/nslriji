461e2a316d6b3f24eca415ea2a6287eb855490c1
dietrich <dietrich@newskylabs.net>
2020-01-02 20:20:04 +0100

    Added Templates

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
+    user = {'username': 'Dietrich'}
+    posts = [
+        {
+            'author': {'username': 'Peter'},
+            'body': 'Hello from New York!'
+        },
+        {
+            'author': {'username': 'Maria'},
+            'body': "Let's meet in Paris!"
+        },
+        {
+            'author': {'username': 'Nina'},
+            'body': "Great trip to China!"
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
