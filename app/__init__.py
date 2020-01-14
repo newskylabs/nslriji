2ce4b4a96d882a113c28e4a0184bf3454d374ba9
dietrich <dietrich@newskylabs.net>
2020-01-14 19:20:10 +0100

    Added SQLite database, User and Post models, migration engine, and some first migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,23 @@
 
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# Application
 app = Flask(__name__)
+
+# Use the application config
 app.config.from_object(Config)
 
-from app import routes
+# Database
+db = SQLAlchemy(app)
 
+# Database migration engine
+migrate = Migrate(app, db)
+
+# Routes and models 
+# have to be imported at the end to avoid circular imports
+from app import routes, models
+
+# fin.
--- config.py
+++ config.py
@@ -1,6 +1,18 @@
 
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
+
+    # Secret key - should be taken from the environment variable
+    # SECRET_KEY in production!
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'very-very-secret-indeed'
 
+    # Location of the application database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    # No need to notify the application
+    # every time a change is about to be made in the database.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
