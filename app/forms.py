b2a8a6b38ccb6c838a232101b61a686c31b4c162
dietrich <dietrich@newskylabs.net>
2020-01-22 18:58:31 +0100

    Fixed Duplicate Username Bug: Show an error message when user tries to rename his username and the new username is already in use.

--- app/forms.py
+++ app/forms.py
@@ -46,6 +46,21 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            # NOTE: 
+            # When two or more processes are accessing the database at
+            # the same time a race condition could cause the
+            # validation to pass...
+            # Very unlikely, so I'm not going to worry about it for now.
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Username already taken. Please try a different one.')
+
 
 ## =========================================================
 ## =========================================================
