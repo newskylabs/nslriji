fce22054263ed8cf0d9da9928b5c71063b344472
dietrich <dietrich@newskylabs.net>
2020-01-29 18:38:52 +0100

    Updated post listing on user pages

--- app/routes.py
+++ app/routes.py
@@ -162,14 +162,18 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
 
     # No error was raised - so the user exists
-    # Get her posts
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    # Get her posts for the requested page
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
 
     # Render user page
-    return render_template('user.html', user=user, posts=posts)
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
