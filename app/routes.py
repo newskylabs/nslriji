a1d04dd0959467b960ea75a7ed6f00faf505b402
dietrich <dietrich@newskylabs.net>
2020-01-29 15:59:55 +0100

    Added post submission form in index view function

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,8 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.models import User, Post
 
 @app.before_request
 def before_request():
@@ -15,10 +15,17 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'Peter'},
@@ -33,8 +40,8 @@ def index():
             'body': "Great trip to China!"
         }
     ]
-    return render_template('index.html', title='Home Page', posts=posts)
-
+    return render_template("index.html", title='Home Page', form=form, posts=posts)
+        
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # Cooperates with load_user() function 
