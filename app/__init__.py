85ccaa0a702a4e72b4184c03fd585cdd2f6de98e
dietrich <dietrich@newskylabs.net>
2020-02-04 16:51:03 +0100

    Converted base.html to be based on bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 from config import Config
 
@@ -32,6 +33,9 @@ login.login_view = 'login'
 
 # Flask-Mail instance
 mail = Mail(app)
+
+# Flask-Bootstrap
+bootstrap = Bootstrap(app)
 
 # ==================
 # Set up logging
