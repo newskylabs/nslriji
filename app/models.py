f948e5b7589a9024a2480691608703567a1e3428
dietrich <dietrich@newskylabs.net>
2020-02-05 12:03:05 +0100

    Minor cleanup

--- app/models.py
+++ app/models.py
@@ -9,13 +9,6 @@ import jwt
 from app import app, db, login
 
 
-@login.user_loader
-def load_user(id):
-    """A function to load the User required by 
-    flask_login's LoginManager.
-    """
-    return User.query.get(int(id))
-
 
 # Auxiliary table 'followers'
 # to define the many-to-many relationship between 
@@ -49,14 +42,14 @@ class User(UserMixin, db.Model):
         lazy='dynamic'
     )
 
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
 
     def avatar(self, size):
         """Get the avator for the user registered at 'Gravatar'
@@ -127,6 +120,14 @@ class User(UserMixin, db.Model):
 
         return User.query.get(id)
     
+
+@login.user_loader
+def load_user(id):
+    """A function to load the User required by 
+    flask_login's LoginManager.
+    """
+    return User.query.get(int(id))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
