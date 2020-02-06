f733e620ee7e31c2641cbf66b5a712a946237d7f
dietrich <dietrich@newskylabs.net>
2020-02-06 17:47:38 +0100

    Added command-line tools for babel translations: flask translate init|update|compile - see flask translate --help

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
-from flask_babel import _
+from flask_babel import _, get_locale
 
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, \
@@ -18,6 +18,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 
 @app.route('/', methods=['GET', 'POST'])
